cb699ae7615eb817d75f3b38599e018e8ced505f
OctavioMaia <octaviojmaia@gmail.com>
2016-06-14 17:34:27 +0100

    DAO de atividade começado

--- VisualStudio/BackOffice/BackOffice/Data/DataBase/AtividadeDAO.cs
+++ VisualStudio/BackOffice/BackOffice/Data/DataBase/AtividadeDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackOffice.Business;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -14,6 +15,101 @@ namespace BackOffice.Data.DataBase
         public AtividadeDAO(string conf)
         {
             this.dbConf = conf;
+        }
+
+
+
+        public List<int> keySet()
+        {
+            List<int> l = null;
+            SqlConnection con = new SqlConnection(this.dbConf);
+            con.Open();
+            //con.BeginTransaction();
+            try
+            {
+                l = this.keySet(con);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return l;
+        }
+
+
+        public Atividade get(int id)
+        {
+            Atividade a = null;
+            SqlConnection con = new SqlConnection(this.dbConf);
+            con.Open();
+            try
+            {
+                a = this.get(id, con);
+            }
+            finally
+            {
+
+                con.Close();
+            }
+            return a;
+        }
+
+        public List<Atividade> Values()
+        {
+            List<Atividade> l = null;
+            SqlConnection con = new SqlConnection(this.dbConf);
+            con.Open();
+            // con.BeginTransaction();
+            try
+            {
+                l = this.Values(con);
+            }
+            finally
+            {
+
+                con.Close();
+            }
+            return l;
+        }
+
+        public Boolean containsKey(int id)
+        {
+            Boolean b = false;
+            SqlConnection con = new SqlConnection(this.dbConf);
+            con.Open();
+            //con.BeginTransaction();
+            try
+            {
+                b = this.containsKey(id, con);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return b;
+        }
+
+        public int size()
+        {
+            return this.keySet().Count;
+        }
+
+        public Atividade put(Atividade novo)
+        {
+            Atividade b = null;
+
+            SqlConnection con = new SqlConnection(this.dbConf);
+            con.Open();
+            //con.BeginTransaction();
+            try
+            {
+                b = this.put(novo, con);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return b;
         }
 
         private List<int> keySet(SqlConnection connection)
@@ -38,21 +134,51 @@ namespace BackOffice.Data.DataBase
         }
 
 
-        public List<int> keySet()
+        private Boolean containsKey(int id, SqlConnection connection)
         {
-            List<int> l = null;
-            SqlConnection con = new SqlConnection(this.dbConf);
-            con.Open();
-            //con.BeginTransaction();
-            try
-            {
-                l = this.keySet(con);
-            }
-            finally
-            {
-                con.Close();
-            }
-            return l;
+            Boolean ret = false;
+            Atividade b = this.get(id, connection);
+            ret = (b != null);
+            return ret;
         }
+
+        private List<Atividade> Values(SqlConnection connection)
+        {
+            List<Atividade> b = new List<Atividade>();
+            List<int> k = this.keySet(connection);
+            foreach (int i in k)
+            {
+                b.Add(this.get(i, connection));
+            }
+            return b;
+        }
+
+        //TODO daqui para baixo
+
+        private Atividade put(Atividade novo, SqlConnection connection)
+        {
+            Atividade b = this.get(novo.idAtividade, connection);
+            String queryString=null;
+            if (b == null) //inserie
+            {
+                
+            }
+            else//update
+            {
+                
+            }
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.CommandTimeout = 60;
+            command.ExecuteNonQuery();
+            return b;
+        }
+
+
+        private Atividade get(int id, SqlConnection connection)
+        {
+            Atividade a = null;
+            return a;
+        }
+
     }
 }
