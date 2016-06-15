313afcddbd5bc7f8862212b398003b445886f358
beatrizaarao <beatrizaarao@MacBook-Pro-de-Beatriz-4.local>
2016-06-15 19:47:51 +0100

    imagens

--- JustTrailIt/app/src/main/java/rallyscouts/justtrailit/activity/LoginActivity.java
+++ JustTrailIt/app/src/main/java/rallyscouts/justtrailit/activity/LoginActivity.java
@@ -67,6 +67,7 @@ public class LoginActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.setTitle("");
         Log.d("TESTE","merda");
         dbAdapter = new DBAdapter(LoginActivity.this);
         batedores = new BatedorDAO(LoginActivity.this);
