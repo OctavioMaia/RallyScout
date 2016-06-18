a5b006b7342718f7d18a4601df572bb10e02ef17
Rui Freitas <rui4825@gmail.com>
2016-06-18 02:57:22 +0100

    Threads terminavam a meio
    Penso que esteja corrigido o erro em que as threads terminavam a meio
    porque a main estava muito sobrecarregada por causa do join que se
    fazia para depois actualizar os butoes

--- JustTrailIt/app/src/main/java/rallyscouts/justtrailit/activity/MenuBatedor.java
+++ JustTrailIt/app/src/main/java/rallyscouts/justtrailit/activity/MenuBatedor.java
@@ -1,6 +1,7 @@
 package rallyscouts.justtrailit.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -101,11 +102,10 @@ public class MenuBatedor extends AppCompatActivity {
 
         if(ipServer!=null && portServer!=-1){
 
-            Thread thread = new Thread(new Runnable()
-            {
+
+            AsyncTask taskDownload = new AsyncTask() {
                 @Override
-                public void run()
-                {
+                protected Object doInBackground(Object[] params) {
                     try {
                         JSONObject request = JsonRC.downloadAtividade(batedorLogin.getEmail(),batedorLogin.getPassword());
 
@@ -126,47 +126,45 @@ public class MenuBatedor extends AppCompatActivity {
                     } catch (IOException e) {
 
                     }
+                    return null;
                 }
-            });
 
-            thread.start();
-            try {
-                thread.join();
-                if(batedorLogin.getAtividade()>=0) {
-                    textView_AtividadeDisp.setText("Atividade " + batedorLogin.getAtividade() + " não enviada");
-                    this.button_gerirAtividade.setEnabled(true);
-                    this.button_download.setEnabled(false);
-                    this.button_upload.setEnabled(true);
-                }else{
-                    if(batedorLogin.getAtividade()==-1){
-                        Toast.makeText(getApplicationContext(), "Não existe atividade disponivel" , Toast.LENGTH_LONG).show();
+                @Override
+                protected void onPostExecute(Object o) {
+                    super.onPostExecute(o);
+                    if(batedorLogin.getAtividade()>=0) {
+                        textView_AtividadeDisp.setText("Atividade " + batedorLogin.getAtividade() + " não enviada");
+                        button_gerirAtividade.setEnabled(true);
+                        button_download.setEnabled(false);
+                        button_upload.setEnabled(true);
+                    }else{
+                        if(batedorLogin.getAtividade()==-1){
+                            Toast.makeText(getApplicationContext(), "Não existe atividade disponivel" , Toast.LENGTH_LONG).show();
+                        }
                     }
+
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            };
+
+            taskDownload.execute();
         }
     }
 
-    public void uploadAtividade(View v){
+    public void uploadAtividade(View v) {
 
 
-        Toast.makeText(getApplicationContext(), "Comunicação ainda não está a funcionar" + portServer , Toast.LENGTH_LONG).show();
-        if(ipServer!=null && portServer!=-1) {
+        if (ipServer != null && portServer != -1) {
 
-            Thread thread = new Thread(new Runnable() {
+            AsyncTask taskUpload = new AsyncTask() {
                 @Override
-                public void run() {
-
+                protected Object doInBackground(Object[] params) {
                     JSONObject send = JsonRC.sendAtividade(batedorLogin, notas.getAllNotas(batedorLogin.getAtividade()));
-
                     Log.i(TAG, send.toString());
-                    //Toast.makeText(getApplicationContext(), send.toString() , Toast.LENGTH_LONG).show();
 
                     try {
                         Socket socket = new Socket(ipServer, portServer);
 
-                        BufferedReader br = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
                         bw.write(send.toString());
@@ -174,44 +172,46 @@ public class MenuBatedor extends AppCompatActivity {
                         bw.flush();
 
 
-
                         String jsonACK = br.readLine();
 
                         int ack = JsonRC.reciveACK(jsonACK);
 
-                        if(ack>=0){
+                        if (ack == -2) {
                             notas.deleteAllNotaAtividade(batedorLogin.getAtividade());
                             veiculos.deleteAllVeiculoAtividade(batedorLogin.getAtividade());
                             mapas.deleteMapa(batedorLogin.getAtividade());
                             atividades.deleteAtividade(batedorLogin.getAtividade());
                             batedorLogin.setAtividade(-1);
-                        }else{
-                            Log.i(TAG,"Não foi enviada corretamente a atividade");
+                            Log.i(TAG, "Atividade enviada");
+                        } else {
+                            Log.i(TAG, "Não foi enviada corretamente a atividade");
                         }
 
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    return null;
                 }
-            });
 
-            thread.start();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                @Override
+                protected void onPostExecute(Object o) {
+                    super.onPostExecute(o);
+                    if (batedorLogin.getAtividade() == -1) {
+                        button_gerirAtividade.setEnabled(false);
+                        button_download.setEnabled(true);
+                        button_upload.setEnabled(false);
+                        textView_AtividadeDisp.setText("Não existe nenhuma atividade em processamento");
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Não foi enviada corretamente a atividade", Toast.LENGTH_LONG).show();
+                    }
 
-            if(batedorLogin.getAtividade()==-1){
-                this.button_gerirAtividade.setEnabled(false);
-                this.button_download.setEnabled(true);
-                this.button_upload.setEnabled(false);
-                textView_AtividadeDisp.setText("Não existe nenhuma atividade em processamento");
-            }else {
-                Toast.makeText(getApplicationContext(), "Não foi enviada corretamente a atividade" , Toast.LENGTH_LONG).show();
-            }
+                }
+            };
+
+            taskUpload.execute();
+        }else {
+            Toast.makeText(getApplicationContext(), "Comunicação ainda não está a funcionar" + portServer, Toast.LENGTH_LONG).show();
         }
-
     }
 
     public void configConnection(View v){
